52fdcb0ac4898073582fcdb4bd4788381a3972c0
root <root@localhost.localdomain>
2023-05-30 11:51:48 +0000

    Se arreglo un error #8

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,38 +1,19 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function get(context) {
   
-  const mysql = await getCollection('mysql').catch(() => []);
-  const discord = await getCollection('discord').catch(() => []);
-  const mongodb = await getCollection('mongodb').catch(() => []);
-  const javascript = await getCollection('javascript').catch(() => []);
-  const html = await getCollection('html').catch(() => []);
-  const css = await getCollection('css').catch(() => []);
-  const python = await getCollection('python').catch(() => []);
-  const java = await getCollection('java').catch(() => []);
-  
-  const allDocs = [
-      ...mysql,
-      ...discord,
-      ...mongodb,
-      ...javascript,
-      ...html,
-      ...css,
-      ...python,
-      ...java,
-      ];
+  const docImportResult = import.meta.glob('../content/**/*.md', { eager: true });
+  const docs = Object.values(docImportResult);
   
   return rss({
     title: 'Documentacion Mundodev',
     description: "Una web de documentaiones de/para la comunidad",
     site: context.site,
-    items: allDocs.map((doc) => ({
+    items: docs.map((doc) => ({
       link: `/docs/${doc.data.tag}/${doc.slug}`,
-      content: sanitizeHtml(parser.render(doc.body)),
+      content: sanitizeHtml(doc.compiledContent()),
       ...doc.data
     })),
   });
