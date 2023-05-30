4667ec499ffe20d876b259f8c67217358a0031c0
root <root@localhost.localdomain>
2023-05-30 12:21:12 +0000

    Se agrego rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,40 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function get(context) {
+  
+  const mysql = await getCollection('mysql').catch(() => []);
+const discord = await getCollection('discord').catch(() => []);
+const mongodb = await getCollection('mongodb').catch(() => []);
+const javascript = await getCollection('javascript').catch(() => []);
+const html = await getCollection('html').catch(() => []);
+const css = await getCollection('css').catch(() => []);
+const python = await getCollection('python').catch(() => []);
+const java = await getCollection('java').catch(() => []);
+
+const allDocs = [
+    ...mysql,
+    ...discord,
+    ...mongodb,
+    ...javascript,
+    ...html,
+    ...css,
+    ...python,
+    ...java,
+    ];
+  
+  return ({
+    title: 'Documentacion Mundodev',
+    description: 'Obten doumentaciones de toda la comunidad de Mundodev o otras',
+    site: context.site,
+    items: allDocs.map((doc) => ({
+      title: doc.data.title,
+      description: doc.data.description,
+      author: doc.data.author,
+      image: 
+        url: doc.data.image.url,
+        alt: doc.data.image.alt,
+      link: `/docs/${post.data.tag}/${post.slug}`
+    })),
+  });
+}
