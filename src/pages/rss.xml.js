cb001a972d991d362988d796cbdddb4e47e412b3
root <root@localhost.localdomain>
2023-05-30 12:58:10 +0000

    Se agrego otro archivo de prueba

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,6 +7,6 @@ export async function get(context) {
     title: 'Documentacion Mundodev',
     description: 'Obten doumentaciones de toda la comunidad de Mundodev o otras',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./content/**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
   });
 }
